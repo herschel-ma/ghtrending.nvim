2f5e6df1c893a796771ca853fa108d75a0e25d86
herschel-ma <maliang_gs@126.com>
2025-02-04 23:41:30 +0800

    feat(refactor): refactor architecture of the project.

--- crates/ghtrending/src/lib.rs
+++ crates/ghtrending/src/lib.rs
@@ -1,3 +1,4 @@
+use mlua::prelude::*;
 use mlua::Lua;
 use mlua::UserData;
 use scraper::{Html, Selector};
@@ -349,4 +350,37 @@ pub async fn process_devloper() -> Result<Vec<UserDataType>, Box<dyn Error>> {
     let data: Vec<Developer> = serde_json::from_value(data)?;
     let result: Vec<UserDataType> = data.into_iter().map(UserDataType::Developer).collect();
     return Ok(result);
+}
+
+fn process<F>(lua: &Lua, f: F) -> LuaResult<LuaTable>
+where
+    F: Fn() -> Result<Vec<UserDataType>, Box<dyn std::error::Error>>,
+{
+    let datas = f()
+        .map_err(|e| mlua::Error::RuntimeError(format!("Error: {}", e)))
+        .unwrap();
+    let array_table = lua.create_table()?;
+
+    for (i, data) in datas.into_iter().enumerate() {
+        match data {
+            UserDataType::Repository(repo) => array_table.set(i + 1, repo)?,
+            UserDataType::Developer(dev) => array_table.set(i + 1, dev)?,
+        };
+    }
+    Ok(array_table)
+}
+
+#[mlua::lua_module]
+fn ghtrending_nvim(lua: &Lua) -> LuaResult<LuaTable> {
+    let exports = lua.create_table()?;
+    let process_devloper = lua.create_function(|lua, ()| {
+        process(lua, process_devloper).map_err(|err| mlua::Error::RuntimeError(err.to_string()))
+    })?;
+    let process_repo = lua.create_function(|lua, ()| {
+        process(lua, process_repo).map_err(|err| mlua::Error::RuntimeError(err.to_string()))
+    })?;
+
+    exports.set("process_developer", process_devloper)?;
+    exports.set("process_repo", process_repo)?;
+    Ok(exports)
 }
